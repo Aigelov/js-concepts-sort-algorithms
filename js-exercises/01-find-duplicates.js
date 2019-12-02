c869f87656d619623e5a4df169b00addfc2435d7
Aigelov <aigelov@gmail.com>
2019-12-02 19:01:34 +0600

    Find duplicates

--- js-exercises/01-find-duplicates.js
+++ js-exercises/01-find-duplicates.js
@@ -1,10 +1,11 @@
-const arr = [1, 2, 3, 4, 5, 6, 3];
+const arr = [1, 2, 3, 4, 5, 6, 3, 4, 4, 5, 5, 9, 10, 12, 13, 1, 1, 1, 2, 2, 6, 4];
 const arr2 = [];
 
 let hasDuplicate = false;
 for (let elem of arr) {
   if (arr2.includes(elem)) {
     hasDuplicate = true;
+    continue;
   }
   arr2.push(elem);
 }
