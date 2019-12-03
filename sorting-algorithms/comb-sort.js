62f068cf61ad399e42aece453df2c146143ab568
Aigelov <aigelov@gmail.com>
2019-12-03 13:22:46 +0600

    Comb sort

--- sorting-algorithms/comb-sort.js
+++ sorting-algorithms/comb-sort.js
@@ -1,41 +1,21 @@
 const arr = require('./generate-random-numbers').randomNumbers;
 let counter = 0;
-let delta = Math.ceil(arr.length / 2);
 
-const sortArr = (arr) => {
-  let stopIteration = true;
-  while (stopIteration) {
-    let isSorted = true;
-    for (let i = 0; i < arr.length - delta; i++) {
-      counter++;
-      if (arr[i] > arr[i + delta]) {
-        [arr[i], arr[i + delta]] = [arr[i + delta], arr[i]];
-        isSorted = false;
+const sortArr = arr => {
+  const length = arr.length;
+  const factor = 1.247;
+  let gapFactor = length / factor;
+  while (gapFactor > 1) {
+    const gap = Math.round(gapFactor);
+    for (let i = 0, j = gap; j < length; i++, j++) {
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+        counter++;
       }
     }
-
-    if (delta > 1) {
-      delta--;
-    }
-
-    if (isSorted) {
-      isSorted = isSortedFn(arr);
-    }
-
-    if (isSorted) {
-      stopIteration = false;
-    }
+    gapFactor /= factor;
   }
-};
-const isSortedFn = (arr) => {
-  let isSorted = true;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i + 1]) {
-      isSorted = false;
-      break;
-    }
-  }
-  return isSorted;
+  return arr;
 };
 console.log(`Array length: ${arr.length}`);
 sortArr(arr);
