dcec229e7c6cba2311d34a321feec05b91771b4f
Aigelov <aigelov@gmail.com>
2019-12-03 12:56:27 +0600

    Shaker sort

--- sorting-algorithms/shaker-sort.js
+++ sorting-algorithms/shaker-sort.js
@@ -1,30 +1,26 @@
 const arr = require('./generate-random-numbers').randomNumbers;
 let counter = 0;
-
 const sortArr = (arr) => {
-  let stopIteration = false;
-  let whileCounter = 0;
-  while (!stopIteration) {
-    let isSorted = true;
-    for (let i = whileCounter; i < arr.length - 2; i++) {
-      counter++;
+  let isSorted = false;
+  let iterator = 0;
+  while (!isSorted) {
+    isSorted = true;
+    for (let i = iterator; i < arr.length; i++) {
       if (arr[i] > arr[i + 1]) {
         [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
         isSorted = false;
+        counter++;
       }
     }
 
-    for (let i = arr.length - whileCounter - 1; i > 0; i--) {
-      counter++;
-      if (arr[i] < arr[i - 1]) {
-        [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
+    for (let j = arr.length - iterator - 1; j >= iterator; j--) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         isSorted = false;
+        counter++;
       }
     }
-
-    if (isSorted) {
-      stopIteration = true;
-    }
+    iterator++;
   }
 };
 console.log(`Array length: ${arr.length}`);
